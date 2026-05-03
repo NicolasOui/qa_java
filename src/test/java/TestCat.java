9671daff2dde51933222bfdbb4e0338f73f71ca8
Nicolas <qellylight@gmail.com>
2026-05-03 11:12:49 +0300

    Добавлены юнит-тесты для классов Lion, Cat и Feline

---
+++ src/test/java/TestCat.java
@@ -0,0 +1,40 @@
+import com.example.Cat;
+import com.example.Feline;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TestCat {
+
+    @Mock
+    Feline feline;
+
+    @Test
+    public void CatGetSound() {
+        Cat cat = new Cat(feline);
+        assertEquals("Мяу", cat.getSound());
+    }
+
+    @Test
+    public void CatgetFoodReceiveFoodFelinie() throws Exception {
+        List<String> FoodForCat = List.of("Зебра", "Барсук");
+        Mockito.when(feline.eatMeat()).thenReturn(FoodForCat);
+        Cat cat = new Cat(feline);
+        assertEquals(FoodForCat, cat.getFood());
+        Mockito.verify(feline).eatMeat();
+    }
+}
+
+
+
+
+
+
+
